ec3bb595353287398e35876da826a1e01af83a38
ingo-weber <ingo.weber@tu-berlin.de>
2021-01-25 17:10:11 +0100

    Update after lecture

--- src/de/tuberlin/sbe/prog1winf/exceptions/ExceptionBasics.java
+++ src/de/tuberlin/sbe/prog1winf/exceptions/ExceptionBasics.java
@@ -3,23 +3,31 @@ package de.tuberlin.sbe.prog1winf.exceptions;
 import Prog1Tools.IOTools;
 
 public class ExceptionBasics {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MyOwnException {
         try {
             int res = divide();
             System.out.println(res);
-        } catch (ArithmeticException e ){
-            System.out.println("Though oughta not divide by zero");
+        } catch (ArithmeticException | MyOwnException e ){
+            if (e instanceof ArithmeticException) {
+                System.out.println("Though oughta not divide by zero");
+            }
             System.out.println(e.getMessage());
             e.printStackTrace();
+//            throw e;
         }
+
+        System.out.println("Calculation completed");
     }
 
-    private static int divide() throws ArithmeticException {
-//        int divident = IOTools.readInt("Divident: ");
-//        int divisor = IOTools.readInt("Divisor: ");
-        int divident = 273;
-        int divisor = 0;
+    private static int divide() throws ArithmeticException, MyOwnException {
+        int divident = IOTools.readInt("Divident: ");
+        int divisor = IOTools.readInt("Divisor: ");
+//        int divident = 273;
+//        int divisor = 0;
         int res = divident/divisor;
+        if (divisor > 0) {
+            throw new MyOwnException("My custom message");
+        }
         return res;
     }
 }
---
+++ src/de/tuberlin/sbe/prog1winf/exceptions/MyOwnException.java
@@ -0,0 +1,8 @@
+package de.tuberlin.sbe.prog1winf.exceptions;
+
+public class MyOwnException extends Exception {
+
+    public MyOwnException(String str) {
+        super(str);
+    }
+}
